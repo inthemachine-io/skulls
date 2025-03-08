7c1a305f0f9f298f5091f5c9c97ba1e730522122
saxmode <human@inthemachine.io>
2025-03-08 23:29:57 +0100

    Update Skulls_Fetch_Detail.js

--- docs/Script/Skulls_Fetch_Detail.js
+++ docs/Script/Skulls_Fetch_Detail.js
@@ -67,11 +67,12 @@ async function fetchData(id) {
         document.getElementById("skull-color-style").innerText = Skull_Color_Style;
         document.getElementById("skull-prompt-style").innerText = Skull_Prompt_Style;
         document.getElementById("skull-engine").innerText = Skull_EngineUsed;
-        document.getElementById("skull-artist").innerText = Skull_Artist_Name + " (" + Skull_Artist_Alive + ")";
+        document.getElementById("skull-artist").innerText = Skull_Artist_Name + "\n(" + Skull_Artist_Alive + ")";
         document.getElementById("skull-artist-style").innerText = Skull_Artist_Style;
         document.getElementById("skull-art-movement").innerText = Skull_Art_Movement + " (" + Skull_Art_Movement_Period + ")";
         document.getElementById("skull-artist-bio").innerText = Skull_Artist_Biography;
         document.querySelector("#skull-artist-link a").href = Skull_Artist_Wikipedia;
+        document.querySelector("#skull-artist-link a").target = "_blank";
 
     } catch (error) {
         console.error("❌ Fehler beim Laden der Daten:", error);
