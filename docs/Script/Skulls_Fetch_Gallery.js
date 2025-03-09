9e2152940df92f40616c2f9066c9938a5018c29c
saxmode <human@inthemachine.io>
2025-03-09 12:48:15 +0100

    Update Skulls_Fetch_Gallery.js

--- docs/Script/Skulls_Fetch_Gallery.js
+++ docs/Script/Skulls_Fetch_Gallery.js
@@ -25,7 +25,7 @@ async function fetchData() {
         const skulls = json.table.rows.slice(0, 100).map(row => ({
             image: row[Index_Skull_Image]?.v ? String(row[Index_Skull_Image].v) : "https://inthemachine-io.github.io/skulls/146-991693-AS.png",
             detailURL: row[Index_Skull_Detail_URL]?.v ? String(row[Index_Skull_Detail_URL].v) : "#",
-            artist: row[Index_Skull_Artist]?.v ? String(row[Index_Skull_Artist].v) : "Unknown Artist",
+            artist: row[Index_Skull_Artist_Name]?.v ? String(row[Index_Skull_Artist].v) : "Unknown Artist",
         }));
 
         if (skulls.length === 0) {
@@ -50,7 +50,7 @@ async function fetchData() {
                         console.log(skulls[index].image);
                         console.log(skulls[index].detailURL);
                         console.log(skulls[index].artist);
-                        
+
                         img.src = skulls[index].image;
                         img.onerror = () => {
                             img.src = "https://inthemachine-io.github.io/skulls/146-991693-AS.png"; // Fallback-Bild
